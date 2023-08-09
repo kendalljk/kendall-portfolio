c65d9fced32d50d5ffe69fbcedaefac4ac72ee8d
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-08-09 13:40:06 -0700

    add icons for linkedIn, github

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,13 @@
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+
 export default function Contact() {
-  <h1>Contact</h1>
+    return (
+        <div className="flex flex-col min-h-screen align-middle justify-around">
+            <h1 className="text-center">Contact</h1>
+            <div className="flex justify-center gap-5 text-teal-900">
+                <FaLinkedin size={40} />
+                <FaGithubSquare size={40} />
+            </div>
+        </div>
+    );
 }
