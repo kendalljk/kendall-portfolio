0e701b855f3df41f0819b2c743dcbe5335f52b73
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-12-14 06:38:27 -0600

    update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,16 +18,8 @@ export default function About() {
                     <p className="my-2">
                         <span className="font-bold text-apricot">{`Hi, I'm Kendall. `}</span>{" "}
                         {`I'm a Full Stack Web
-                  Developer with a unique blend of educational and professional
-                  experiences. With a Bachelor's degree in English and numerous
-                  years working in the health field, I bring a strong foundation
-                  in communication and analytical thinking to the world of web
-                  development. My experience instilled in me a deep
-                  understanding of the importance of user-centered design and
-                  functionality, especially in creating digital solutions that
-                  can positively impact people's lives`}
+                  Developer with a a diverse and synergistic skill set. What truly drives me in the field of web development is the sense of satisfaction from building something from scratch. I have a particular affinity for front-end development, reveling in the challenge of bringing designs to life and fine-tuning the user experience. The 'aha' moments in coding, when a complex problem yields to a solution, are what keeps me always striving to learn more.`}
                     </p>
-                    <p className="my-2">{`What truly drives me in the field of web development is the sense of satisfaction from building something from scratch. I have a particular affinity for front-end development, reveling in the challenge of bringing designs to life and fine-tuning the user experience. The 'aha' moments in coding, when a complex problem yields to a solution, are what keeps me always striving to learn more.`}</p>
                 </div>
             </div>
         </div>
